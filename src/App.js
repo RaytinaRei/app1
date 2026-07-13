3245ce0c2cdc6e1b8425716b96f53976cef2110d
RaytinaRei <nolive.tnt@gmail.com>
2026-07-13 11:17:11 +0700

    Update App.js and style.css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
-
+import './style.css'; 
 function App() {
-    const divstyle ={
-      color: 'red',
-      backgroundColor: 'powderblue',
-      fontSize: 'larger',
-      padding: '3px',
-    }
   
   
   return (
-    <div style={divstyle}>
-      <h1>Hello React</h1>
-    </div>
+   <>
+    <h2>DeveloperThai.com</h2>
+    <div className="title">React & React Native</div>
+   </>
   );
 }
 
